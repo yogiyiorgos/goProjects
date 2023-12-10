6b6f116cd6fd0269e36f14db74e0e824cd5e4b8e
YogiYiorgos <yiorgosdev@gmail.com>
2023-12-10 19:16:14 +0100

    GP Go-REST Dec10

---
+++ go-rest-api/internal/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/yogiyiorgos/go-rest-api/pkg/swagger/server/restapi"
+	"github.com/yogiyiorgos/go-rest-api/pkg/swagger/server/restapi/operations"
+)
+
+func main() {
+	// Initialize swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			// error handle
+			log.Fatalln(err)
+		}
+	}()
+
+	server.PORT = 8080
+
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+	// Start server\
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Health route returns OK
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+// GetGelloUser returns Hello + your name
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+}
+
+// GetGopherByName returns a gopher in png
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		// The default return is dr-who gopher
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
+
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
+}
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Println("Listening on localhost: 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
